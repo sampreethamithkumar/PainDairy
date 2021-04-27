65406ddad0dc6e95f4658e4732ff0ad3ced0f038
sampreethamithkumar <sampreethamith65@gmail.com>
2021-04-27 18:49:42 +1000

    Retrieve Login User data on Home Screen

--- app/src/main/java/com/example/paindairy/fragment/HomeFragment.java
+++ app/src/main/java/com/example/paindairy/fragment/HomeFragment.java
@@ -4,15 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.paindairy.Dashboard;
+import com.example.paindairy.User;
 import com.example.paindairy.databinding.HomeFragmentBinding;
 import com.example.paindairy.fragment.viewmodel.SharedViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment {
-    private SharedViewModel model;
+//    private SharedViewModel model;
     private HomeFragmentBinding addBinding;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
+
+    private FirebaseAuth mAuth;
+
     public HomeFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +39,8 @@ public class HomeFragment extends Fragment {
         // Inflate the View for this fragment
         addBinding = HomeFragmentBinding.inflate(inflater, container, false);
         View view = addBinding.getRoot();
+        mAuth = FirebaseAuth.getInstance();
+        getUserDetails();
         return view;
     }
     @Override
@@ -27,5 +48,32 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         addBinding = null;
     }
+    private void getUserDetails() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
 
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+
+                    addBinding.emailIdTextView.setText(email);
+                    addBinding.fullNameTextView.setText(fullName);
+                    addBinding.ageTextView.setText(age);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Something Wrong happened", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
