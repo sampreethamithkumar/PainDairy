793778ee29dfc7b004c8f8963ca5ee22628a7fe5
sampreethamithkumar <sampreethamith65@gmail.com>
2021-05-04 11:58:40 +1000

    Initial Implementation of pie chart

--- app/src/main/java/com/example/paindairy/fragment/PieChartFragment.java
+++ app/src/main/java/com/example/paindairy/fragment/PieChartFragment.java
@@ -7,13 +7,46 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.example.paindairy.R;
 import com.example.paindairy.databinding.AccountFragmentBinding;
+import com.example.paindairy.databinding.FragmentPieBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PieChartFragment extends Fragment {
+    private FragmentPieBinding pieBinding;
+
+    private String[] months = {"Jan" , "Feb", "March"};
+    private int[] earning = {200,300,400};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState) {
-       return inflater.inflate(R.layout.fragment_pie,container,false);
+        pieBinding = FragmentPieBinding.inflate(inflater,container,false);
+        View view = pieBinding.getRoot();
+
+        setupPieChart();
+
+       return view;
+    }
+
+    public void setupPieChart() {
+        Pie pie = AnyChart.pie();
+        List<DataEntry> dataEntries = new ArrayList<>();
+
+
+        for (int i = 0; i < months.length; i++) {
+            dataEntries.add(new ValueDataEntry(months[i], earning[i]));
+        }
+
+        pie.data(dataEntries);
+        pieBinding.anyChartView.setChart(pie);
+
     }
 }
